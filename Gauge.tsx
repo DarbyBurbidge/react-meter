646341138d7f319cab29d6206df72347fd27f243
DarbyBurbidge <darby.burbidge@gmail.com>
2021-12-30 02:45:26 -0800

    got dynamically filled section done based on the number of breakpoints (the section has the right fill amount, but dynamically set). Still need to make sure it works for all sections

--- Gauge.tsx
+++ Gauge.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
-import "./Styles/Gauge.css"
+import parse from "html-react-parser";
+import "./Styles/Gauge.css";
 
 interface Props {
     value: string;
     breakpoints?: string
+    posX: string
+    posY: string
 }
 
 export const Gauge: React.FC<Props> = (props) => {
@@ -12,6 +15,7 @@ export const Gauge: React.FC<Props> = (props) => {
     const [ meters, setMeters ] = useState({})
     const [ display, setDisplay ] = useState({})
     const breakpoints = [numBreakpoints]
+    let gaugeSVG: string = ''
     let meterStateString = 'setMeters({ '
     for(let i = 0; i < numBreakpoints; i++) {
         breakpoints[i] = ((i+1)/(numBreakpoints+1));
@@ -19,10 +23,14 @@ export const Gauge: React.FC<Props> = (props) => {
         
 
         if (i == 0) {
-            meterStateString += `...meters, ${`metersVal${i}`}: ` + `${(base > breakpoints[0] ? 1 : (base / breakpoints[0]))}`
+            meterStateString += `...meters, ${`meterVal${i}`}: ` + `${(base > breakpoints[0] ? 1 : (base / breakpoints[0]))}`
+            gaugeSVG += `<circle id=meter-${i} cx=${props.posX} cy=${props.posY} r="114.6"></circle>` +
+                        `<circle id='meter-${i} meter-fg-${i}' cx=${props.posX} cy=${props.posY} r="114.6"></circle>`
         }
+        // gaugeSVG += `<circle id=${`meter-${i+1}`} cx=${`${props.posX}`} cy=${`${props.posY}`} r="114.6"></circle>
+        //             <circle id=${`meter-${i+1} meter-fg-${i+1}`} cx=${`${props.posX}`} cy=${`${props.posY}`} r="114.6" style={eval(display.display1 ? {strokeDasharray: ` + '`${meters.meterVal1 * 110}, ${(720 - meters.meterVal1 * 110)}`} : {display: "none"})}></circle>'
         console.log("hmm...")
-        meterStateString += `, ${`metersVal${i+1}`}: ` + `${(base - breakpoints[i] < 0 ?
+        meterStateString += `, ${`meterVal${i+1}`}: ` + `${(base - breakpoints[i] < 0 ?
                 0 : 
             Math.min((base - breakpoints[i]) / breakpoints[0], 1))}`
         
@@ -32,6 +40,21 @@ export const Gauge: React.FC<Props> = (props) => {
     if(Object.keys(meters).length == 0) {
         eval(meterStateString)
     }
+    useEffect(() => {
+        for(let i = 0; i < Object.keys(meters).length; i++) {
+            const circle = document.getElementById(`meter-${i} meter-fg-${i}`)
+            if (circle && Object.keys(meters).length > 0) {
+                if (/*eval(`display.display${i}`)*/ true) {
+                    const meterEval = eval(`meters.meterVal${i}`)
+                    const strokeLength = eval(`${meterEval} * 110`)
+                    const strokeGap = eval('(720 - `${meterEval}` * 110)')
+                    circle.setAttribute("style", `stroke-dasharray: ${strokeLength}, ${strokeGap}`)
+                }
+                console.log(`meter-fg-${i}`)
+                //true ?  : {display: `none`}
+            }
+        }
+    });
     /*const val1 = base > breakpoints[0] ? 1 : (base / breakpoints[0]);
     const val2 = base - breakpoints[0] < 0 ? 0 : Math.min((base - breakpoints[0]) / breakpoints[0], 1);
     const val3 = base - breakpoints[1] < 0 ? 0 : Math.min((base - breakpoints[1]) / breakpoints[0], 1);*/
@@ -42,14 +65,16 @@ export const Gauge: React.FC<Props> = (props) => {
         display2: (meters.meterVal2 > 0 && base > 0),
         display3: (meters.meterVal3 > 0 && base > 0)
     })*/
-    console.log(breakpoints)
-    console.log(meters);
+    console.log(true ? '{strokeDasharray: `${33}` * 110, (720 - `${33}` * 110)}' : '{display: `none`}')
+
    
     return(
         <div className="gauge">
             <div className="percent">
-            <svg>
-                {/*<circle id="meter-1" cx="300" cy="300" r="114.6"></circle>
+            <svg id="svg">
+                {parse(gaugeSVG)}
+                {/*
+                <circle id="meter-1" cx="300" cy="300" r="114.6"></circle>
                 <circle id="meter-1 meter-fg-1" cx="300" cy="300" r="114.6" style={display.display1 ? {strokeDasharray: `${meters.meterVal1 * 110}, ${(720 - meters.meterVal1 * 110)}`} : {display: 'none'}}></circle>
                 <circle id="meter-2" cx="300" cy="300" r="114.6"></circle>
                 <circle id="meter-3 meter-fg-2" cx="300" cy="300" r="114.6" style={display.display2 ? {strokeDasharray: `${meters.meterVal2 * 110}, ${(720 - meters.meterVal2 * 110)}`} : {display: 'none'}}></circle>
